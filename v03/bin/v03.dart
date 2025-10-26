57df3352d46a486857d029844fdf0b4ce1942594
Ziaul Kabir <ziaul.kabir@worldline.com>
2025-10-26 19:08:51 +0100

    Added v03

---
+++ v03/bin/v03.dart
@@ -0,0 +1,5 @@
+import 'package:v03/v03.dart' as v03;
+
+void main(List<String> arguments) {
+  print('Hello world: ${v03.calculate()}!');
+}
---
+++ v03/test/v03_test.dart
@@ -0,0 +1,8 @@
+import 'package:v03/v03.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
